a16637eb547a3f91f6edd4f5d0bbf94d9f0cdeec
shivadeep <laithepena@gmail.com>
2016-12-13 13:45:41 -0800

    Signed-off-by: shivadeep <laithepena@gmail.com>

--- src/audit/PomMain.java
+++ src/audit/PomMain.java
@@ -6,26 +6,30 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class PomMain {
 	public static void main(String[] args) {
 		
-		//SparkConf conf = new SparkConf().setAppName("PompuApp").set("spark.executor.memory", "16g");
+//  SparkConf is common to the whole program - so there is only one conf
+		
+		//SparkConf conf = new SparkConf().setAppName("PompuApp").set("spark.executor.memory", "16g"); // need to deComment this one for Cluster Mode
 		
 		SparkConf conf = new SparkConf().setAppName("PompuApp").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
-       
+        
+        
+ //############ Adrouter ##################################################################################################	  
 		AdrouterData adr=new AdrouterData();
-		
+			
 		DataContext D1=new DataContext(adr);
 		D1.setDataConfig(sc,"logFile_location_adr");
 		D1.executeDataStrategy(sc, "stg_adr");
 		
 	
-//############ FeedBack ##################################################################################################
+//############ FeedBack ###################################################################################################
 	FeedbackData feed=new FeedbackData();
 		
 		DataContext D2=new DataContext(feed);
 		D2.setDataConfig(sc, "logFile_location_feed");
 		D2.executeDataStrategy(sc,"stg_feedback");
 		
-		System.out.println("............DONE DONE DONE.................");
+		System.out.println("............DONE DONE DONE......HAVE A GOOD DAY......DONE DONE DONE.....");
 	}
 
 }
